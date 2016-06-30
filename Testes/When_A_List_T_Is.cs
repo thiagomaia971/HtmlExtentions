de3b45fe1e383f6e061cdc6111e9233bedf76c59
Thiago Maia da Costa <thiago.maia971@gmail.com>
2016-06-30 09:21:43 -0300

    Project Testes Added

---
+++ Testes/When_A_List_T_Is.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using HtmlExtentions.Interfaces;
+using Core;
+using System.Collections.Generic;
+
+namespace Testes
+{
+    [TestClass]
+    public class When_A_List_T_Is
+    {
+
+        Mock<ITableList<Usuario>> mockTableList;
+        ITableList<Usuario> tableList;
+
+        public When_A_List_T_Is()
+        {
+
+            mockTableList = new Mock<ITableList<Usuario>>();
+            tableList = mockTableList.Object;
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Added_A_UsuarioIsNull_ThrowsArgumentNullException()
+        {
+
+            // Arrange
+
+            Usuario usuario = null;
+
+            mockTableList.Setup(u => u.List).Returns(new List<Usuario>());
+            mockTableList.Setup(u => u.AddToList(usuario)).Throws<ArgumentNullException>();
+
+            // Act
+
+            tableList.AddToList(usuario);
+
+            // Assert
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Added_A_IColletionUsuarioIsNull_ThrowsNullReferenceException()
+        {
+
+            //Arrange
+
+            List<Usuario> usuarios = null;
+
+            mockTableList.Setup(mTableList => mTableList.List).Returns(new List<Usuario>());
+            mockTableList.Setup(mTableList => mTableList.AddToList(usuarios)).Throws<ArgumentNullException>();
+
+            //Act
+
+            tableList.AddToList(usuarios);
+
+            //Assert
+
+        }
+
+        [TestMethod]
+        public void Added_A_Usuario_ListCountOneMore()
+        {
+
+            // Arrange
+
+            Usuario usuario = new Usuario()
+            {
+                Id = 1,
+                Login = "dragus12",
+                Nome = "Thiago Maia"
+            };
+
+            mockTableList.Setup(mTableList => mTableList.List).Returns(new List<Usuario>() { usuario });
+            mockTableList.Setup(mTableList => mTableList.AddToList(usuario));
+
+            // Act
+
+            tableList.AddToList(usuario);
+
+            //Assert
+
+            mockTableList.Verify(mTableList => mTableList.AddToList(usuario), Times.Exactly(1));
+
+            mockTableList.Verify();
+
+        }
+
+    }
+}
---
+++ Testes/When_A_PropertiesToShow_Is.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core;
+using HtmlExtentions.Interfaces;
+using Moq;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+
+namespace Testes
+{
+    [TestClass]
+    public class When_A_PropertiesToShow_Is
+    {
+
+        Mock<ITableList<Usuario>> mockTableList;
+        ITableList<Usuario> tableList;
+
+        public When_A_PropertiesToShow_Is()
+        {
+
+            mockTableList = new Mock<ITableList<Usuario>>();
+            tableList = mockTableList.Object;
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Added_A_PropertyToShowIsNull_ThrowsArgumentNullException()
+        {
+
+            // Arrange
+
+            Expression<Func<Usuario, object>> property = null;
+
+            mockTableList.Setup(u => u.List).Returns(new List<Usuario>());
+            mockTableList.Setup(u => u.AddPropertyToShow(property)).Throws<ArgumentNullException>();
+
+            // Act
+
+            tableList.AddPropertyToShow(property);
+
+            // Assert
+
+        }
+
+    }
+}
